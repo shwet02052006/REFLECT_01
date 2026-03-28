631a0bef0b67d4b1a1df5788ceb3cfc4acf0706a
shwet02052006 <shwet.9834@gmail.com>
2026-03-28 19:55:21 +0530

    fix

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api'
+  baseURL: import.meta.env.VITE_API_URL
 });
-
 api.interceptors.request.use((config) => {
   const userStr = localStorage.getItem('reflection_user');
   if (userStr) {
